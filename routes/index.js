123287b80f280273e48bb87ddd0c8706c8af36b5
Cyrill von Wattenwyl <cvw@adfinis.com>
2015-09-07 13:17:16 +0200

    new page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Openshift' });
 });
 
+router.get('/foo', function(req, res, next) {
+  res.render('foo', { title: 'Openshift' });
+});
+
 module.exports = router;
